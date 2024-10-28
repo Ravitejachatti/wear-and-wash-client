f17538a9e331671e2c49990e980a9bc956e57a73
Ravitejachatti <amma10701@gmail.com>
2024-10-28 10:47:30 +0530

    security api keys hidden

--- Components/Config/firebase.js
+++ Components/Config/firebase.js
@@ -4,15 +4,15 @@ import { getDatabase } from "firebase/database";
 
 // Your Firebase configuration object (you can find this in your Firebase console)
 const firebaseConfig = {
-    apiKey: "AIzaSyCWzoPFTl-edN-ud3hXfBrzK33KcqVsDhY",
-    authDomain: "wearnwash-95e48.firebaseapp.com",
+    apiKey: "your api key",
+    authDomain: "auth domain",
     databaseURL:
-      "https://wearnwash-95e48-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "wearnwash-95e48",       
-    storageBucket: "wearnwash-95e48.appspot.com",
-    messagingSenderId: "101431081714",
-    appId: "1:101431081714:web:3f501ba6c24428a8ed2e6b",
-    measurementId: "G-1F3XCEVHER",
+      "datebase url",
+    projectId: "your project id",       
+    storageBucket: "y",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   };
 
 // Initialize Firebase app
